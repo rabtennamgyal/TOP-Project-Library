0fa00375dac4e504d4a908be1fc1e88d9058e9df
Rabten <rabten@protonmail.com>
2021-10-11 05:42:17 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+const inputCard = document.querySelector('.inputCard')
+const newBookBtn = document.getElementById('newBook')
+const submit = document.querySelector('.submit') 
+const mainCard = document.getElementById('mainCard')
+
+let input1 = document.getElementById('titleValue')
+let input2 = document.getElementById('authorValue')
+let input3 = document.getElementById('pagesValue')
+
+
+function displayInputCard() {
+    inputCard.style.display = 'grid';
+}
+
+let library = localStorage.getItem('Book') ? JSON.parse(localStorage.getItem('Book')) : []
+
+
+function book(title, author, pages) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+}
+
+let title, author, pages
+
+function createCard(title, author, pages) {
+    const bookCard = document.createElement('div')
+    bookCard.classList.add('divStyle')
+    bookCard.innerHTML = `
+    <h1><span>Title: </span>${title}</h1>
+    <h1><span>Author: </span>${author}</h1>
+    <h1><span>Pages: </span>${pages}
+    <div id='btnBox'>
+        <button class='check'>✅ Read</button>
+        <button class='delete'>❌ Delete</button>
+    </div>
+    `;
+    bookCard.addEventListener('click', function(e) {
+        if (e.target.classList.contains('check')) {
+            bookCard.style.background = '#1cf31c'
+        }
+        if (e.target.classList.contains('delete')) {
+            library.splice(bookCard, 1)
+            bookCard.parentNode.removeChild(bookCard)
+        }
+    })
+    mainCard.appendChild(bookCard)
+    console.log(localStorage)
+    console.log(library)
+}
+
+
+function addBookToLibrary(e) {
+    if (input1.value === '' || input2.value === '' || input3.value === '') {
+        return
+    } else {
+        e.preventDefault()
+        title = document.getElementById('titleValue').value
+        author = document.getElementById('authorValue').value
+        pages = document.getElementById('pagesValue').value
+
+
+        let newBook = new book(title, author, pages)
+        library.push(newBook)
+        localStorage.setItem('Book', JSON.stringify(library))
+        createCard(title, author, pages)
+
+        inputCard.style.display = 'none'
+        input1.value = ''
+        input2.value = ''
+        input3.value = ''
+    }
+}
+
+library.forEach(el => {
+    createCard(el.title, el.author, el.pages)
+})
+
+newBookBtn.addEventListener('click', displayInputCard)
+submit.addEventListener('click', addBookToLibrary)
+
+
+
+
+
+

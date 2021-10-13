a74445d1094485a07d4f969e685fa2f02862c262
Rabten <rabten@protonmail.com>
2021-10-12 23:58:42 -0400

    done with all functionality

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mainCard = document.getElementById('mainCard')
 let input1 = document.getElementById('titleValue')
 let input2 = document.getElementById('authorValue')
 let input3 = document.getElementById('pagesValue')
+let input4 = document.getElementById('Yes')
+let input5 = document.getElementById('No')
 
 function displayInputCard() {
     inputCard.style.display = 'grid';
@@ -13,21 +15,23 @@ function displayInputCard() {
 
 let library = localStorage.getItem('Book') ? JSON.parse(localStorage.getItem('Book')) : []
 
-function book(title, author, pages) {
+function book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
+    this.read = read
 }
 
-let title, author, pages
+let title, author, pages, read
 
-function createCard(title, author, pages) {
+function createCard(title, author, pages, read) {
     const bookCard = document.createElement('div')
     bookCard.classList.add('divStyle')
     bookCard.innerHTML = `
-    <h1 classList='content'>${title}</h1>
-    <h1 classList='content'>${author}</h1>
-    <h1 classList='content'>${pages}</h1>
+    <h1>${title}</h1>
+    <h1>${author}</h1>
+    <h1>${pages}</h1>
+    <h1>${read}</h1>
     <div id='btnBox'>
         <button class='check'>✅ Read</button>
         <button class='delete'>❌ Delete</button>
@@ -35,7 +39,22 @@ function createCard(title, author, pages) {
     `;
     bookCard.addEventListener('click', function(e) {
         if (e.target.classList.contains('check')) {
-            bookCard.style.background = '#1cf31c'
+            library = JSON.parse(localStorage.getItem('Book'))
+
+            let parentEl = e.target.parentNode.parentNode
+            let a = parentEl.childNodes[1].textContent
+            let b = parentEl.childNodes[3].textContent
+            let c = parentEl.childNodes[5].textContent
+            let d = parentEl.childNodes[7].textContent
+
+            for (let i = 0; i < library.length; i++) {
+                if (library[i].title === a && library[i].author === b && library[i].pages === c) {
+                    let x = library.indexOf(library[i])
+                    library[x].read = 'I already read it.'
+                    localStorage.setItem('Book', JSON.stringify(library))
+                    location.reload()
+                }
+            }
         }
         if (e.target.classList.contains('delete')) {
             library = JSON.parse(localStorage.getItem('Book'))
@@ -69,11 +88,16 @@ function addBookToLibrary(e) {
         title = document.getElementById('titleValue').value
         author = document.getElementById('authorValue').value
         pages = document.getElementById('pagesValue').value
+        if (input4.checked) {
+            read = 'I already read it.'
+        } else {
+            read = 'Didn\'t read it yet.'
+        }
 
-        let newBook = new book(title, author, pages)
+        let newBook = new book(title, author, pages, read)
         library.push(newBook)
         localStorage.setItem('Book', JSON.stringify(library))
-        createCard(title, author, pages)
+        createCard(title, author, pages, read)
 
         inputCard.style.display = 'none'
         input1.value = ''
@@ -83,7 +107,7 @@ function addBookToLibrary(e) {
 }
 
 library.forEach(el => {
-    createCard(el.title, el.author, el.pages)
+    createCard(el.title, el.author, el.pages, el.read)
 })
 
 newBookBtn.addEventListener('click', displayInputCard)
